1209774174ac8d3f6412dc3e9e7be435ec050104
Joe <jakatsa5@gmail.com>
2023-12-19 19:49:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+/**** */
+const employee = {
+  name: "joe",
+  streetAddress: "race",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  const newEmployee = { ...employee };
+  newEmployee[key] = value;
+  return newEmployee;
+}
+const updateEmployee = updateEmployeeWithKeyAndValue(employee, "name", "mike");
+/*console.log(updateEmployee)*/
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+
+const newEmployee = destructivelyUpdateEmployeeWithKeyAndValue(
+  employee,
+  "name",
+  "akatsa"
+);
+/*console.log(newEmployee);*/
+function deleteFromEmployeeByKey(employee, key) {
+  const deleteEmployee = { ...employee };
+  delete deleteEmployee[key];
+  return deleteEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee[key];
+  return employee;
+}
